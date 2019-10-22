369ecbb7c9e11949a998e0360fd8a49c6f70cf63
Stheffany <stheffanyhadlich@gmail.com>
2019-10-21 22:00:08 -0300

    Intro to components lifecycles

--- src/App.js
+++ src/App.js
@@ -1,49 +1,75 @@
-import './App.css';
+import "./App.css";
 
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   state = {
-    name: '',
-    list:[]
-  }
+    name: "",
+    list: [],
+    show: true
+  };
 
   onChange = event => {
     const { value } = event.target;
-    this.setState( () => ({
+    this.setState(() => ({
       name: value
     }));
-  }
-  
+  };
+
   onClick = () => {
-    this.setState(()=>({
-      name: '',
+    this.setState(() => ({
+      name: "",
       list: [...this.state.list, this.state.name]
-    }))
-  }
+    }));
+  };
 
   onDelete = value => () => {
     this.setState(() => ({
-      list: this.state.list.filter(item => item !== value)
-    }))
+      list: this.state.list.filter(item => item !== value),
+      show: false
+    }));
+  };
+
+  componentDidMount() {
+    console.log("Component did mount");
   }
 
-  render () {
-    const { name, list } = this.state;
+  render() {
+    const { name, list, show } = this.state;
 
     return (
       <div>
-        <input type="text" value={name} onChange={this.onChange}/>
-        <button type="button" onClick={this.onClick}>Add</button>
-        <List list={list} onDelete={this.onDelete}/>
+        <input type="text" value={name} onChange={this.onChange} />
+        <button type="button" onClick={this.onClick}>
+          Add
+        </button>
+        {show && <List list={list} onDelete={this.onDelete} />}
       </div>
-    )
+    );
   }
 }
 
-const List = ({list, onDelete}) => {
-  const items = list.map(value => <li key={value}>{value} <button type="button" onClick={onDelete(value)}>X</button></li>);
-  return <ul>{items}</ul>
+class List extends Component {
+  componentWillUnmount() {
+    console.log("Unmount");
+  }
+
+  render() {
+    const { list, onDelete } = this.props;
+
+    const items = list.map(value => (
+      <li key={value}>
+        {value}
+        <button type="button" onClick={onDelete(value)}>
+          X
+        </button>
+      </li>
+    ));
+
+    return (
+      <ul>{items}</ul>
+    );
+  }
 }
 
 export default App;
