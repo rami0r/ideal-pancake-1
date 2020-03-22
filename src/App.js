d74d79e9932026594a7fa918c8ae0bad1eb71210
Stheffany Hadlich <stheffany@codeminer42.com>
2020-03-22 18:58:35 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Alert from "react-bootstrap/Alert";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import Home from "./view/Home";
+import Quiz from "./view/Quiz";
 import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <Alert variant="primary">
-        Hello, world!
-      </Alert>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/quiz" exact component={Quiz} />
+      </Switch>
+    </Router>
   );
 }
 
