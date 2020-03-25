c2a122a82fefb9d305953726211ad73029888b74
Stheffany Hadlich <stheffany@codeminer42.com>
2020-03-24 22:31:23 -0300

    Create Home component

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,5 +1,57 @@
+import { Component } from 'react'
+import { Link } from "react-router-dom";
 import React from 'react'
 
-const Home = () => <div> Home </div>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+    }
+  }
+
+  onChange = (event) => {
+    const { name, value } = event.target
+
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <main className="Page">
+        <header className="Header">
+          <h3> Home </h3>
+        </header>
+        <section className="Container">
+          <div className="Container Form">
+            <label className="Form__Label">
+              Name
+            </label>
+            <input
+              className="Form__Input"
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.onChange}
+            />
+            <Link
+              className="Form__Button"
+              to={{
+                pathname: "/quiz",
+                state: { name }
+              }}
+            >
+              Play
+            </Link>
+          </div>
+        </section>
+      </main>
+    )
+  }
+
+}
 
 export default Home
