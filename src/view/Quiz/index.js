bc5d426b694eb619789354c0f62d73f766c29cb9
Stheffany Hadlich <stheffany@codeminer42.com>
2020-03-24 22:31:43 -0300

    Create Quiz component

--- src/view/Quiz/index.js
+++ src/view/Quiz/index.js
@@ -1,5 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Quiz = () => <div> Quiz </div>
+import { getPokemons } from '../../models/PokemonModel';
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      score: 0,
+      questions: [],
+      index: 0,
+      // questions: [{
+      //   rightAnswer: { name: 'pikachu', url: 'imagem'},
+      //   options: ['pikachu', 'bubalsauro', 'cachorro', 'batata']
+      // }]
+    }
+  }
+
+  componentDidMount() {
+    this.setQuestions();
+  }
+
+  setQuestions = async () => {
+    const questions = await getPokemons();
+    this.setState({
+      questions
+    })
+  }
+
+  render() {
+    const { score, questions, index } = this.state;
+    const { name } = this.props.location.state;
+
+    const question = questions[index];
+
+    return (
+      <main className="Page">
+        <section className="Container">
+          <div className="Container Info">
+            <span> Player: {name} </span>
+            <span> Score: {score} </span>
+          </div>
+          {
+            questions.length > 0 ? (
+              <div className="Container Game">
+                <div className="Game__Question">
+                  {question.rightAnswer.name}
+                </div>
+                <div className="Game__Options">
+                  {
+                    question.options.map(option =>
+                      <span>
+                        { option }
+                      </span>
+                    )
+                  }
+                </div>
+              </div>
+            ) : (
+                <span>
+                  loading...
+                </span>
+              )
+          }
+        </section>
+      </main>
+    )
+  }
+}
 
 export default Quiz
